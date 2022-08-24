b3b59876253a64e00f2125063f7fe54a72d159cb
mickey-40 <mickeyarnold53@yahoo.com>
2022-08-24 18:53:07 -0500

    added edit.ejs, index.ejs, new.ejs, show.ejs

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -18,6 +18,27 @@ router.get('/:id',getJob,(req,res)=>{
   res.send(res.job.title)
 })
 //create one
+// NEW
+router.get('/new', (req, res) => {
+	res.render('new.ejs');
+});
+
+// SHOW
+router.get('/:id', async (req, res) => {
+	// we must wait for this to finish before running any further lines of code in this function
+	const job = await Job.findById(req.params.id);
+	res.render('show.ejs', {
+		job: job,
+	});
+
+	// Fruit.findById(req.params.id, (error, fruit) => {
+	// 	res.render('show.ejs', {
+	// 		fruit: fruit,
+	// 	});
+	// })
+});
+
+
 router.post('/', async (req, res) => {
 	if (req.body.interview === 'on') {
 		req.body.interview = true;
@@ -35,14 +56,19 @@ router.post('/', async (req, res) => {
 	});
 });
 //update one
-router.patch('/:id',(req,res)=>{
-  
+// EDIT
+router.get('/:id/edit', (req, res) => {
+	Job.findById(req.params.id, (err, foundJob) => {
+		res.render('edit.ejs', {job: foundJob})
+	})
 })
 //deleting one
-router.delete('/:id',(req,res)=>{
-  
+router.delete('/:id', (req, res) => {
+	Job.findByIdAndRemove(req.params.id, (err, data)=> {
+		if(err) console.log(err)
+		res.redirect('/jobs')
+	})
 })
-
 async function getJob(req, res, next){
   job = await Job.findById(req.params.id)
   if (job == null){
