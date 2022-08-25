15ad402c3e5e15c8d42a2a220164b901df1dcbad
mickey-40 <mickeyarnold53@yahoo.com>
2022-08-24 20:08:50 -0500

    routes for edit index new and show work

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Job = require('../models/job.js')
 
-//get all
+//Index
 router.get('/', async (req,res)=>{
   
     let jobs = await Job.find({})
@@ -11,34 +11,20 @@ router.get('/', async (req,res)=>{
  
     
 })
-
-
-//get one
-router.get('/:id',getJob,(req,res)=>{
-  res.send(res.job.title)
-})
-//create one
 // NEW
 router.get('/new', (req, res) => {
 	res.render('new.ejs');
 });
-
 // SHOW
 router.get('/:id', async (req, res) => {
-	// we must wait for this to finish before running any further lines of code in this function
+	
 	const job = await Job.findById(req.params.id);
 	res.render('show.ejs', {
 		job: job,
 	});
 
-	// Fruit.findById(req.params.id, (error, fruit) => {
-	// 	res.render('show.ejs', {
-	// 		fruit: fruit,
-	// 	});
-	// })
 });
-
-
+//Create
 router.post('/', async (req, res) => {
 	if (req.body.interview === 'on') {
 		req.body.interview = true;
@@ -55,29 +41,41 @@ router.post('/', async (req, res) => {
 		}
 	});
 });
-//update one
-// EDIT
-router.get('/:id/edit', (req, res) => {
-	Job.findById(req.params.id, (err, foundJob) => {
-		res.render('edit.ejs', {job: foundJob})
-	})
-})
-//deleting one
+//destroy
 router.delete('/:id', (req, res) => {
 	Job.findByIdAndRemove(req.params.id, (err, data)=> {
 		if(err) console.log(err)
 		res.redirect('/jobs')
 	})
 })
-async function getJob(req, res, next){
-  job = await Job.findById(req.params.id)
-  if (job == null){
-    let job
-    return res.status(404).json({message: 'Cannot find job'})
-  }
+// EDIT
+router.get('/:id/edit', (req, res) => {
+	Job.findById(req.params.id, (err, foundJob) => {
+		res.render('edit.ejs', {job: foundJob})
+	})
+})
+//update 
+router.put('/:id', (req, res) => {
+	if(req.body.interview === 'on') {
+		req.body.interview = true
+	} else {
+		req.body.interview = false
+	}
+	Job.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedModel) => {
+		res.redirect('/jobs')
+	})
+})
 
-  res.job = job
-  next()
-}
+
+// async function getJob(req, res, next){
+//   job = await Job.findById(req.params.id)
+//   if (job == null){
+//     let job
+//     return res.status(404).json({message: 'Cannot find job'})
+//   }
+
+//   res.job = job
+//   next()
+// }
 
 module.exports = router
